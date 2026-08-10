0a8d63be567b96348e754d140f60294c38f0f8fd
Aryan1387ar <miladafzhoul14@gmail.com>
2026-08-10 17:20:00 +0330

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,36 +1,50 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
 
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true
+        password: {
+            type: String,
+            required: true
+        },
+
+        active: {
+            type: Boolean,
+            default: true
+        },
+
+        expiresAt: {
+            type: Date,
+            required: true
+        },
+
+        // وضعیت سه دکمه
+        enabled: {
+            set1: {
+                type: Boolean,
+                default: false
+            },
+
+            set2: {
+                type: Boolean,
+                default: false
+            },
+
+            set3: {
+                type: Boolean,
+                default: false
+            }
+        }
     },
-
-    password: {
-        type: String,
-        required: true
-    },
-
-    active: {
-        type: Boolean,
-        default: true
-    },
-
-    expiresAt: {
-        type: Date,
-        required: true
-    },
-
-    enabled: {
-        type: Boolean,
-        default: false
+    {
+        timestamps: true
     }
-
-}, {
-    timestamps: true
-});
+);
 
 module.exports = mongoose.model("User", userSchema);
